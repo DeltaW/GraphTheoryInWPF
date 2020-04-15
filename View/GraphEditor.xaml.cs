a44b998c51065f6f2df1593d5e4fa0f8dfdaa885
Dennis Wiest <dw_8@web.de>
2020-04-15 14:27:36 +0200

    Added the Error

--- View/GraphEditor.xaml.cs
+++ View/GraphEditor.xaml.cs
@@ -31,6 +31,8 @@ namespace GraphTheoryInWPF.View {
 
         private void Button_Click_AddNode(object sender, RoutedEventArgs e) {
             try {
+                if (this.UserInputTextBlock.Text == "")
+                    throw new GraphException("THE NODE NEEDS TO HAVE A VALID NAME!");
                 this._gevm.ButtonAddNode(this.UserInputTextBlock.Text);
                 this.UserInputTextBlock.Text = "";
             } catch (GraphException ge) {
